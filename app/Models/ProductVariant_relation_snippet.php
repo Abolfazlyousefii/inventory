ef0969d8878889cfebfd4e383b5858331cdcdf4d
Abolfazlyousefii <150295740+Abolfazlyousefii@users.noreply.github.com>
2026-07-15 16:57:13 +0330

    Fix persistence and route coverage test setup

--- app/Models/ProductVariant_relation_snippet.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Add this method inside app/Models/ProductVariant.php
-
-public function warehouseStocks()
-{
-    return $this->hasMany(\App\Models\WarehouseStock::class, 'product_variant_id');
-}
