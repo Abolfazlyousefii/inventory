bfa0160b1269951bceed03031d36acb43779dd41
MMD <info@natilos.ir>
2026-07-15 12:57:24 +0330

    delete tests

--- tests/Feature/SalesReturnApplyTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Tests\Feature;
-use App\Models\SalesReturnDocument;
-use Tests\TestCase;
-class SalesReturnApplyTest extends TestCase { public function test_applied_status_constant_is_stable(): void { $this->assertSame('applied', SalesReturnDocument::STATUS_APPLIED); } }
--- tests/Feature/SalesReturnSazehTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Tests\Feature;
-use App\Models\SalesReturnDocument;
-use Tests\TestCase;
-class SalesReturnSazehTest extends TestCase { public function test_sazeh_source_constant_is_stable(): void { $this->assertSame('sazeh_hesab', SalesReturnDocument::SOURCE_SAZEH_HESAB); } }
